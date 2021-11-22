f55b776f9b1424bbd965764590a2dc91c2b0a2cd
Wojtek Kolarczyk <wkolarczyk99@gmail.com>
2021-11-22 16:10:43 +0100

    Page is more responsive.
    Added media query for changing grid structure and for changing fonts-size for better readibility.

--- ContactFormClick.js
+++ ContactFormClick.js
@@ -6,10 +6,12 @@ function showHiddenForm(){
     let clon = temp.content.cloneNode(true)
     if(counter === 0){
         document.getElementsByClassName("footer")[0].appendChild(clon);
+        document.getElementsByClassName("footer")[0].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
         counter = 1;
     }
     else if(counter === 1){
         document.getElementById("formBox").remove();
+        document.getElementsByClassName("contactContent")[0].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
         counter = 0;
     }
 }
--- ProductsCategoriesClick.js
+++ ProductsCategoriesClick.js
@@ -8,10 +8,12 @@ function showContent1(id){
     }
     if(!isVisible){
         document.getElementById(id).style.visibility = "visible";
+        document.getElementById(id).style.height = "auto";
         isVisible = true;
     }
     else{
         document.getElementById(id).style.visibility = "hidden";
+        document.getElementById(id).style.height = "10px";
         isVisible = false;
     }
     console.log(isVisible)
